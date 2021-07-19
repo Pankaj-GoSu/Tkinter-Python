1c2b71ff7d768c4af7c18cf66b0ab73196f76bc2
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-07-19 12:30:30 +0530

    Added tut20 and 21

---
+++ tut20.py
@@ -0,0 +1,20 @@
+#============ Status Bar In Tkinter =======
+
+from tkinter import *
+
+root = Tk()
+root.geometry("455x235")
+root.title("Status bar tutorial")
+
+def upload():
+    statusvar.set("Busy..")
+    sbar.update()
+    import time
+    time.sleep(2)
+    statusvar.set("Ready ")
+statusvar = StringVar()
+statusvar.set("Ready")
+sbar = Label(root,textvariable=statusvar,relief=SUNKEN,anchor="w")
+sbar.pack(side=BOTTOM,fill=X)
+Button(root,text="Upload",command=upload).pack()
+root.mainloop()
